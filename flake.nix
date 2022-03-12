d851ce196ff4da93b08cee849e7d428b1dabf749
hugosenari <hugosenari@gmail.com>
2022-03-12 02:14:28 -0300

    chore: re index oblivion

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''Shell command manager'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."oblivion-main".url = "path:./main";
+  inputs."oblivion-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."oblivion-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."oblivion-v0_1_0".url = "path:./v0_1_0";
+  inputs."oblivion-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."oblivion-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
